3f5f4ea23ec8d7599034fa4bb6b2e1086b8f551d
souvikotaku <manson1989@>
2024-01-03 09:21:03 +0530

    phone name changing on click

--- app/src/main/java/com/example/travelphotoapp/MainActivity.kt
+++ app/src/main/java/com/example/travelphotoapp/MainActivity.kt
@@ -1,19 +1,24 @@
 package com.example.travelphotoapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     private var currentImage = 0
     lateinit var image: ImageView
+    var places = arrayOf("Qutub Minar","India Gate","Red Fort","Opera House")
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val next= findViewById<ImageButton>(R.id.btnNext)
         val prev= findViewById<ImageButton>(R.id.btnPrev)
+        val placeName = findViewById<TextView>(R.id.tVName)
 
         next.setOnClickListener{
             var idCurrentImageString = "pic$currentImage"
@@ -26,6 +31,8 @@ class MainActivity : AppCompatActivity() {
             var idImageToShowInt = this.resources.getIdentifier(idImageToShowString,"id",packageName)
             image = findViewById(idImageToShowInt)
             image.alpha = 1f
+
+            placeName.text = places[currentImage]
         }
 
         prev.setOnClickListener{
@@ -38,6 +45,8 @@ class MainActivity : AppCompatActivity() {
             var idImageToShowInt = this.resources.getIdentifier(idImageToShowString,"id",packageName)
             image = findViewById(idImageToShowInt)
             image.alpha = 1f
+
+            placeName.text = places[currentImage]
         }
 
     }
